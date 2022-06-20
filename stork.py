96d4ff894725b4b95585860862ad5604b2b46a8d
LINEN0723 <95272358+LINEN0723@users.noreply.github.com>
2022-06-20 13:54:00 +0800

    Create stork.py

---
+++ stork.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+from abc import ABC, abstractmethod
+class Stok(ABC):
+    def __init__(self, area):
+        self.area = area
+ # 地區
+    @abstractmethod
+    def scrape(self):
+        pass
+class IFoodie(Stok):
+    def scrape(self):
+        url = ('https://invest.cnyes.com/twstock/TWS/'+self.area)   # 台積電 Yahoo 股市網址
+        web = requests.get(url)                          # 取得網頁內容
+        soup = BeautifulSoup(web.text, "html.parser")
+        content =""    # 轉換內容
+        title=soup.find(class_='jsx-2715122309 header_second')  .getText()         
+        # 找到 h1 的內容
+        a=(soup.find( class_='jsx-2214436525 info-lp')).getText()    
+        b=(soup.find( class_='jsx-2214436525 change-net')).getText()
+        c=soup.find( class_='jsx-2214436525 change-percent').getText()
+        content +=(f'{title}\n股價:\n{a}\n{b}\n{c} ')
+        return content

02dc7be18a33541cfc4aaeca11b564635cbb4bde
LINEN0723 <95272358+LINEN0723@users.noreply.github.com>
2022-06-27 15:36:45 +0800

    Update stork.py

--- stork.py
+++ stork.py
@@ -4,18 +4,16 @@ from abc import ABC, abstractmethod
 class Stok(ABC):
     def __init__(self, area):
         self.area = area
- # 地區
     @abstractmethod
     def scrape(self):
         pass
 class IFoodie(Stok):
     def scrape(self):
         url = ('https://invest.cnyes.com/twstock/TWS/'+self.area)  
-        web = requests.get(url)                          # 取得網頁內容
+        web = requests.get(url)                        
         soup = BeautifulSoup(web.text, "html.parser")
-        content =""    # 轉換內容
-        title=soup.find(class_='jsx-2715122309 header_second')  .getText()         
-        # 找到 h1 的內容
+        content =""   
+        title=soup.find(class_='jsx-2715122309 header_second')  .getText()       
         a=(soup.find( class_='jsx-2214436525 info-lp')).getText()    
         b=(soup.find( class_='jsx-2214436525 change-net')).getText()
         c=soup.find( class_='jsx-2214436525 change-percent').getText()
